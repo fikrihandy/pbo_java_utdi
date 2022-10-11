0efe01c2361eefc1b8d91611dd1acd8403abfb4a
Abdullah Fikri Handi Saputra <abdullahfikrihandi@gmail.com>
2022-10-11 16:32:02 +0700

    Modul 6 - Enkapsulasi dan Komposisi

---
+++ src/id/ac/utdi/modul6/tugas/Buku.java
@@ -0,0 +1,31 @@
+package id.ac.utdi.modul6.tugas;
+
+class Buku {
+    private String judul;
+    private int harga;
+    private int halaman;
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public int getHalaman() {
+        return halaman;
+    }
+
+    public void setHalaman(int halaman) {
+        this.halaman = halaman;
+    }
+}
---
+++ src/id/ac/utdi/modul6/tugas/ObyekBuku.java
@@ -0,0 +1,27 @@
+package id.ac.utdi.modul6.tugas;
+
+public class ObyekBuku {
+    public static void main(String[] args) {
+        Buku buku1 = new Buku();
+
+        // akses field dengan setter
+        buku1.setJudul("Belajar PBO");
+        buku1.setHarga(75000);
+        buku1.setHalaman(200);
+
+        // method tampil() untuk menampilkan data private
+        tampil(buku1);
+
+        Buku buku2 = new Buku();
+        buku2.setJudul("Pengenalan Kotlin");
+        buku2.setHarga(80000);
+        buku2.setHalaman(175);
+        tampil(buku2);
+    }
+
+    private static void tampil(Buku buku) {
+        System.out.println("Judul buku :" + buku.getJudul());
+        System.out.println("Harga buku : " + buku.getHarga());
+        System.out.println("Jumlah halaman : " + buku.getHalaman() + "\n");
+    } // method tampil() untuk menampilkan method dengan getter
+}
