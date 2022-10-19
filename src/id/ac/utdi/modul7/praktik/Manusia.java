bf9741ddb3aa52d897f26ef9104d2ca1f343e4a7
Abdullah Fikri Handi Saputra <abdullahfikrihandi@gmail.com>
2022-10-19 18:23:12 +0700

    Modul 7 - Pewarisan

---
+++ src/id/ac/utdi/modul7/praktik/Manusia.java
@@ -0,0 +1,17 @@
+package id.ac.utdi.modul7.praktik;
+
+class Manusia {
+    protected String nama;
+    protected int umur;
+
+    public Manusia() {
+        this.nama = "Sastro Wardoyo";
+        this.umur = 50;
+    }
+
+    public void cetak() {
+        System.out.println("======DATA PERSONAL====");
+        System.out.println("Nama:" + nama);
+        System.out.println("Umur:" + umur);
+    }
+}
