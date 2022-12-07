d52d6df5cf27fa5fd4338f38fe426fd03a92728e
fikrihandy <abdullahfikrihandi@gmail.com>
2022-12-07 14:36:16 +0700

    Modul 11 - Polimorfisme

---
+++ src/id/ac/utdi/modul11/praktik/Test2.java
@@ -0,0 +1,25 @@
+package id.ac.utdi.modul11.praktik;
+
+class Induk {
+    int x = 5;
+
+    public void Info() {
+        System.out.println("Ini class Induk");
+    }
+}
+
+class Anak extends Induk {
+    int x = 10;
+
+    public void Info() {
+        System.out.println("Ini class Anak");
+    }
+}
+
+public class Test2 {
+    public static void main(String[] args) {
+        Induk tes = new Anak();
+        System.out.println("Nilai x = " + tes.x);
+        tes.Info();
+    }
+}
